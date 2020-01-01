f6a7a397733428dbd20956974ca2f01aa65b24dc
RHP043 <57178433+RHP043@users.noreply.github.com>
2020-01-01 11:17:21 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var database, playerCount;
+var gameState = 0;
+var form,game,player;
+var allPlayers,distance=0;
+var cars,car1,car2,car3,car4;
+
+
+
+function setup(){
+    canvas = createCanvas(displayWidth-20,displayHeight-30);
+    database = firebase.database();
+    game = new  Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+
+    if(playerCount===4) {
+        game.update(1);
+
+    }
+    if(gameState===1) {
+        clear()
+        game.play();
+    }
+
+}
+
